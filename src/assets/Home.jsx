775793ec50f27f89ce13941c8cce4c9818bf3727
SALMANULFARIS2010 <151534811+SALMANULFARIS2010@users.noreply.github.com>
2024-08-14 13:27:44 +0530

    RESUME

--- src/assets/Home.jsx
+++ src/assets/Home.jsx
@@ -314,14 +314,7 @@ const Home = () => {
             
             
             
-            // onMouseEnter={e => {
-            //   e.currentTarget.style.transform = 'scale(1.1)';
-            //   e.currentTarget.style.border = '2px solid #3498db'; // or any color and border style of your choice
-            // }}
-            // onMouseLeave={e => {
-            //   e.currentTarget.style.transform = 'scale(1)';
-            //   e.currentTarget.style.border = 'none'; // or the original border style
-            // }}
+          
             
             
             
@@ -339,16 +332,7 @@ const Home = () => {
               boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
               cursor: 'pointer'
             }} 
-            // onMouseEnter={e => e.currentTarget.style.transform = 'scale(1.2)'}
-            // onMouseLeave={e => e.currentTarget.style.transform = 'scale(1)'}
-            // onMouseEnter={e => {
-            //   e.currentTarget.style.transform = 'rotateY(180deg)';
-            //   e.currentTarget.style.transition = 'transform 0.6s ease';
-            // }}
-            // onMouseLeave={e => {
-            //   e.currentTarget.style.transform = 'rotateY(0deg)';
-            //   e.currentTarget.style.transition = 'transform 0.6s ease';
-            // }}
+          
             onMouseEnter={e => {
               e.currentTarget.style.boxShadow = '0 0 15px rgba(0, 0, 0, 0.8), 0 0 25px rgba(0, 0, 0, 0.6)';
               e.currentTarget.style.border = '2px solid rgba(0, 0, 0, 0.8)';
@@ -376,16 +360,7 @@ const Home = () => {
               boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
               cursor: 'pointer'
             }} 
-            // onMouseEnter={e => e.currentTarget.style.transform = 'scale(1.2)'}
-            // onMouseLeave={e => e.currentTarget.style.transform = 'scale(1)'}
-            // onMouseEnter={e => {
-            //   e.currentTarget.style.transform = 'rotateY(180deg)';
-            //   e.currentTarget.style.transition = 'transform 0.6s ease';
-            // }}
-            // onMouseLeave={e => {
-            //   e.currentTarget.style.transform = 'rotateY(0deg)';
-            //   e.currentTarget.style.transition = 'transform 0.6s ease';
-            // }}
+      
             onMouseEnter={e => {
               e.currentTarget.style.boxShadow = '0 0 15px rgba(0, 0, 0, 0.8), 0 0 25px rgba(0, 0, 0, 0.6)';
               e.currentTarget.style.border = '2px solid rgba(0, 0, 0, 0.8)';
@@ -412,16 +387,7 @@ const Home = () => {
               boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
               cursor: 'pointer'
             }} 
-            // onMouseEnter={e => e.currentTarget.style.transform = 'scale(1.2)'}
-            // onMouseLeave={e => e.currentTarget.style.transform = 'scale(1)'}
-            // onMouseEnter={e => {
-            //   e.currentTarget.style.transform = 'rotateY(180deg)';
-            //   e.currentTarget.style.transition = 'transform 0.6s ease';
-            // }}
-            // onMouseLeave={e => {
-            //   e.currentTarget.style.transform = 'rotateY(0deg)';
-            //   e.currentTarget.style.transition = 'transform 0.6s ease';
-            // }}
+          
             onMouseEnter={e => {
               e.currentTarget.style.boxShadow = '0 0 15px rgba(0, 0, 0, 0.8), 0 0 25px rgba(0, 0, 0, 0.6)';
               e.currentTarget.style.border = '2px solid rgba(0, 0, 0, 0.8)';
@@ -448,17 +414,7 @@ const Home = () => {
               boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
               cursor: 'pointer'
             }} 
-            // onMouseEnter={e => {
-            //   e.currentTarget.style.transform = 'scale(1.1)';
-            //   e.currentTarget.style.border = '2px solid #3498db'; // or any color and border style of your choice
-            // }}
-            // onMouseLeave={e => {
-            //   e.currentTarget.style.transform = 'scale(1)';
-            //   e.currentTarget.style.border = 'none'; // or the original border style
-            // }}
-            
-            // onMouseEnter={e => e.currentTarget.style.transform = 'scale(1.2)'}
-            // onMouseLeave={e => e.currentTarget.style.transform = 'scale(1)'}
+          
             onMouseEnter={e => {
               e.currentTarget.style.boxShadow = '0 0 15px rgba(0, 0, 0, 0.8), 0 0 25px rgba(0, 0, 0, 0.6)';
               e.currentTarget.style.border = '2px solid rgba(0, 0, 0, 0.8)';
@@ -485,16 +441,7 @@ const Home = () => {
               boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
               cursor: 'pointer'
             }} 
-            // onMouseEnter={e => e.currentTarget.style.transform = 'scale(1.2)'}
-            // onMouseLeave={e => e.currentTarget.style.transform = 'scale(1)'}
-            // onMouseEnter={e => {
-            //   e.currentTarget.style.transform = 'rotateY(180deg)';
-            //   e.currentTarget.style.transition = 'transform 0.6s ease';
-            // }}
-            // onMouseLeave={e => {
-            //   e.currentTarget.style.transform = 'rotateY(0deg)';
-            //   e.currentTarget.style.transition = 'transform 0.6s ease';
-            // }}
+       
             onMouseEnter={e => {
               e.currentTarget.style.boxShadow = '0 0 15px rgba(0, 0, 0, 0.8), 0 0 25px rgba(0, 0, 0, 0.6)';
               e.currentTarget.style.border = '2px solid rgba(0, 0, 0, 0.8)';
@@ -522,16 +469,7 @@ const Home = () => {
               boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
               cursor: 'pointer'
             }} 
-            // onMouseEnter={e => e.currentTarget.style.transform = 'scale(1.2)'}
-            // onMouseLeave={e => e.currentTarget.style.transform = 'scale(1)'}
-            // onMouseEnter={e => {
-            //   e.currentTarget.style.transform = 'rotateY(180deg)';
-            //   e.currentTarget.style.transition = 'transform 0.6s ease';
-            // }}
-            // onMouseLeave={e => {
-            //   e.currentTarget.style.transform = 'rotateY(0deg)';
-            //   e.currentTarget.style.transition = 'transform 0.6s ease';
-            // }}
+         
             onMouseEnter={e => {
               e.currentTarget.style.boxShadow = '0 0 15px rgba(0, 0, 0, 0.8), 0 0 25px rgba(0, 0, 0, 0.6)';
               e.currentTarget.style.border = '2px solid rgba(0, 0, 0, 0.8)';
@@ -558,17 +496,7 @@ const Home = () => {
               boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
               cursor: 'pointer'
             }} 
-            // onMouseEnter={e => {
-            //   e.currentTarget.style.transform = 'rotateY(180deg)';
-            //   e.currentTarget.style.transition = 'transform 0.6s ease';
-            // }}
-            // onMouseLeave={e => {
-            //   e.currentTarget.style.transform = 'rotateY(0deg)';
-            //   e.currentTarget.style.transition = 'transform 0.6s ease';
-            // }}
-            
-            // onMouseEnter={e => e.currentTarget.style.transform = 'scale(1.2)'}
-            // onMouseLeave={e => e.currentTarget.style.transform = 'scale(1)'}
+         
             onMouseEnter={e => {
               e.currentTarget.style.boxShadow = '0 0 15px rgba(0, 0, 0, 0.8), 0 0 25px rgba(0, 0, 0, 0.6)';
               e.currentTarget.style.border = '2px solid rgba(0, 0, 0, 0.8)';
@@ -595,17 +523,7 @@ const Home = () => {
               boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
               cursor: 'pointer'
             }} 
-            // onMouseEnter={e => {
-            //   e.currentTarget.style.transform = 'rotateY(180deg)';
-            //   e.currentTarget.style.transition = 'transform 0.6s ease';
-            // }}
-            // onMouseLeave={e => {
-            //   e.currentTarget.style.transform = 'rotateY(0deg)';
-            //   e.currentTarget.style.transition = 'transform 0.6s ease';
-            // }}
-            
-            // onMouseEnter={e => e.currentTarget.style.transform = 'scale(1.2)'}
-            // onMouseLeave={e => e.currentTarget.style.transform = 'scale(1)'}
+           
             onMouseEnter={e => {
               e.currentTarget.style.boxShadow = '0 0 15px rgba(0, 0, 0, 0.8), 0 0 25px rgba(0, 0, 0, 0.6)';
               e.currentTarget.style.border = '2px solid rgba(0, 0, 0, 0.8)';
@@ -641,7 +559,8 @@ const Home = () => {
           Contact Me
         </a>
         <a 
-          href="/src/assets/Cvdoc/CV.pdf" 
+          // href="/src/assets/Cvdoc/CV.pdf" 
+          href="../assets/Cvdoc/CV.pdf"
           download="Resume.pdf" 
           style={{ 
             padding: '0.75rem 2rem', 
@@ -661,3 +580,104 @@ const Home = () => {
 
 export default Home;
 
+// import React from 'react';
+// import meImage from './Photo/me.jpeg';  // You can still import this one as it's being used elsewhere
+
+// const Home = () => {
+//   const whatsappNumber = '+918592990768';
+
+//   return (
+//     <section style={{ 
+//       textAlign: 'center', 
+//       height: '100vh', 
+//       width: '100vw', 
+//       display: 'flex', 
+//       flexDirection: 'column', 
+//       justifyContent: 'center', 
+//       alignItems: 'center',
+//       margin: '0', 
+//       padding: '0'
+//     }}>
+//       <div>
+//         <img 
+//           src={meImage}
+//           alt="Profile" 
+//           style={{ borderRadius: '50%', width: '150px', height: '150px' }} 
+//         />
+//       </div>
+//       <h1 style={{ marginTop: '1rem' }}>Hi! I'm Salmanul Faris 👋</h1>
+//       <h3 style={{ marginTop: '1rem', maxWidth: '600px', margin: 'auto' }}>
+//         Dynamic MERN Stack Developer Specializing in <br />
+//         Full-Stack Solutions and Innovative Web Applications
+//       </h3>
+//       <div style={{ marginTop: '2rem', textAlign: 'center' }}>
+//         <h2>SKILLS</h2>
+//         <div style={{ display: 'flex', justifyContent: 'center', gap: '20px', flexWrap: 'wrap' }}>
+//           {[
+//             {
+//               src: "https://e7.pngegg.com/pngimages/185/866/png-clipart-html-logo-html-web-design-scalable-graphics-world-wide-web-markup-language-html5-icon-hd-miscellaneous-angle-thumbnail.png",
+//               alt: "HTML"
+//             },
+//             {
+//               src: "https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg",
+//               alt: "CSS"
+//             },
+//             {
+//               src: "https://customizepress.ca/wp-content/uploads/2017/01/bootstrap-logo.jpg",
+//               alt: "Bootstrap"
+//             },
+//             {
+//               src: "https://miro.medium.com/v2/resize:fit:522/1*NJSv6DGoKTloI8d8im98zg.png",
+//               alt: "React.js"
+//             },
+//             {
+//               src: "https://e7.pngegg.com/pngimages/503/848/png-clipart-javascript-computer-icons-software-developer-cascading-style-sheets-javascript-logo-angle-text-thumbnail.png",
+//               alt: "JavaScript"
+//             },
+//             {
+//               src: "https://www.pngall.com/wp-content/uploads/13/Mongodb-PNG-Image-HD.png",
+//               alt: "MongoDB"
+//             },
+//             {
+//               src: "https://www.pngitem.com/pimgs/m/1-15848_node-js-node-js-hd-png-download.png",
+//               alt: "Node.js"
+//             },
+//             {
+//               src: "https://media.dev.to/cdn-cgi/image/width=1600,height=900,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fi%2Fhpg6if7btrwilqkidqbe.png",
+//               alt: "Express.js"
+//             }
+//           ].map((icon, index) => (
+//             <img 
+//               key={index}
+//               src={icon.src}
+//               alt={icon.alt}
+//               style={{ 
+//                 width: '70px', 
+//                 height: '70px', 
+//                 borderRadius: '15px', 
+//                 backgroundColor: '#fff', 
+//                 padding: '10px',
+//                 transition: 'transform 0.3s ease, box-shadow 0.3s ease',
+//                 boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+//                 cursor: 'pointer'
+//               }} 
+//               onMouseEnter={e => {
+//                 e.currentTarget.style.boxShadow = '0 0 15px rgba(0, 0, 0, 0.8), 0 0 25px rgba(0, 0, 0, 0.6)';
+//                 e.currentTarget.style.border = '2px solid rgba(0, 0, 0, 0.8)';
+//                 e.currentTarget.style.transform = 'scale(1.05)';
+//                 e.currentTarget.style.transition = 'all 0.3s ease';
+//               }}
+//               onMouseLeave={e => {
+//                 e.currentTarget.style.boxShadow = '0 0 5px rgba(0, 0, 0, 0.5)';
+//                 e.currentTarget.style.border = '2px solid rgba(0, 0, 0, 0.5)';
+//                 e.currentTarget.style.transform = 'scale(1)';
+//               }}
+//             />
+//           ))}
+//         </div>
+//       </div>
+//     </section>
+//   );
+// };
+
+// export default Home;
